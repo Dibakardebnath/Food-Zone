a4c8eeb9debede593f55923dea521021cb2f5cab
Dibakardebnath <ddibakar190@gmail.com>
2024-01-23 23:07:01 +0530

    today1

--- food-app/src/Component/AboutUs.jsx
+++ food-app/src/Component/AboutUs.jsx
@@ -1,11 +1,21 @@
 
-import { Box } from '@chakra-ui/react'
+import { Box, Heading,Image,Text } from '@chakra-ui/react'
 
 
 export const AboutUs=()=>{
     return <Box>
 
-      <h1>About Us</h1>
+      <Box className='aboutFirstBox' display={'flex'}>
+
+      <Box>
+        <Heading>Nothing to worry about with Quickeat</Heading>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
+      </Box>
+      <Box>
+        <Image src="https://quickeat-react.vercel.app/assets/img/photo-9.jpg"></Image>
+      </Box>
+
+      </Box>
 
     </Box>
 }

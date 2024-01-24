797815c05e629bc42780ae87715a14ea733138c0
Dibakardebnath <ddibakar190@gmail.com>
2024-01-24 23:28:40 +0530

    tod

--- food-app/src/Component/AboutUs.jsx
+++ food-app/src/Component/AboutUs.jsx
@@ -1,18 +1,21 @@
 
 import { Box, Heading,Image,Text } from '@chakra-ui/react'
-
+import './About.css'
 
 export const AboutUs=()=>{
-    return <Box>
+    return <Box  background-color= {"linear-gradient(135deg,#ffeaea, rgb(255,255,255),#ffeaea, rgb(255,255,255))"}>
 
       <Box className='aboutFirstBox' display={'flex'}>
 
-      <Box>
-        <Heading>Nothing to worry about with Quickeat</Heading>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
+      <Box className='aboutFirstBox1'>
+        <Text color={'gray'} mb={'5%'}>Home <i style={{color:"#f29f05"}} class="fa-solid fa-arrow-right"></i> <span style={{color:"black",  fontWeight:"bold"}}>About Us</span></Text>
+        <Heading mb={'2%'}   w={"70%"} as='h2' size='2xl'>Nothing to worry</Heading>
+        <Heading mb={'5%'}    as='h2' size='2xl'>about with Quickeat</Heading>
+        
+        <Text  w={"70%"} color={"gray"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
       </Box>
-      <Box>
-        <Image src="https://quickeat-react.vercel.app/assets/img/photo-9.jpg"></Image>
+      <Box className='aboutFirstBox2'>
+        <Image   borderRadius={"100px"}  transform="rotate(15deg)"  src="https://quickeat-react.vercel.app/assets/img/photo-9.jpg"></Image>
       </Box>
 
       </Box>

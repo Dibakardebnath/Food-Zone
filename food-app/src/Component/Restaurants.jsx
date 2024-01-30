e9b6e228a0d2ea22d1b31100c67569651f206e64
Dibakardebnath <ddibakar190@gmail.com>
2024-01-30 23:40:44 +0530

    donme

--- food-app/src/Component/Restaurants.jsx
+++ food-app/src/Component/Restaurants.jsx
@@ -5,7 +5,9 @@ import { Box } from '@chakra-ui/react'
 export const Restaurants=()=>{
     return <Box>
 
-      <h1>Restaurenst</h1>
+      <h1>Restaurenst
+        Tommorow come3s your day no wory
+      </h1>
 
     </Box>
 }
